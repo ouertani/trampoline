c6dace06240839bae0c9a248e73ec54856658ded
ouertani <ouertani@gmail.com>
2014-04-01 20:41:22 +0100

    compiles

--- src/main/java/com/technozor/trampoline/Bounce.java
+++ src/main/java/com/technozor/trampoline/Bounce.java
@@ -4,43 +4,58 @@ package com.technozor.trampoline;
 import java.util.function.Supplier;
 
 /**
- *
- * @author ouertani
  * @param <A>
+ * @author ouertani
  */
 public interface Bounce<A> {
-    
-    
-    public static  <A> Done<A> Done(A result) { return new Done(result); }
-    public static  <A> Call<A> Call(Supplier<Bounce<A>> thunk) { return new Call(thunk); }
-    
-    public Boolean isDone();
+
+
+    public static <A> Done<A> Done(A result) {
+        return new Done(result);
+    }
+
+    public static <A> Call<A> Call(Supplier<Bounce<A>> thunk) {
+        return new Call(thunk);
+    }
+
+    public default boolean hasNext() {
+        return true;
+    }
+
     public static <A> A trampoline(final Bounce<A> bounce) {
         Bounce _bounce = bounce;
-        while(!_bounce.isDone())
-           _bounce = ((Call<A>) _bounce).thunk();
+        while (_bounce.hasNext())
+            _bounce = ((Call<A>) _bounce).thunk();
 
-            return((Done<A>) _bounce).thunk();
+        return ((Done<A>) _bounce).thunk();
     }
-    
+
     class Done<A> implements Bounce<A> {
-       private final A result;
-       public Done(A result) {
-           this.result = result;
-       }
-       public A thunk() {return result; }
-       
-       @Override
-       public Boolean isDone(){return true ; }
-    } 
-   
-   class Call<A> implements Bounce<A> {
-       private final Supplier<Bounce<A>> thunk;
-       public Call(Supplier<Bounce<A>> thunk) {
-           this.thunk =thunk;
-       }
-       public Bounce<A> thunk() { return thunk.get(); }
-       @Override
-       public Boolean isDone() { return false ; }
-   }
+        private final A result;
+
+        public Done(A result) {
+            this.result = result;
+        }
+
+        public A thunk() {
+            return result;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return false;
+        }
+    }
+
+    class Call<A> implements Bounce<A> {
+        private final Supplier<Bounce<A>> thunk;
+
+        public Call(Supplier<Bounce<A>> thunk) {
+            this.thunk = thunk;
+        }
+
+        public Bounce<A> thunk() {
+            return thunk.get();
+        }
+    }
 }
--- src/test/java/com/technozor/trampoline/BounceTest.java
+++ src/test/java/com/technozor/trampoline/BounceTest.java
@@ -1,70 +1,68 @@
 
 package com.technozor.trampoline;
 
-import static com.googlecode.catchexception.throwable.CatchThrowable.catchThrowable;
-import static com.googlecode.catchexception.throwable.CatchThrowable.caughtThrowable;
-import static com.technozor.trampoline.Bounce.*;
-import java.math.BigInteger;
-import static org.junit.Assert.assertFalse;
-
-
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ErrorCollector;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
+import java.math.BigInteger;
+
+import static com.googlecode.catchexception.throwable.CatchThrowable.catchThrowable;
+import static com.googlecode.catchexception.throwable.CatchThrowable.caughtThrowable;
+import static com.technozor.trampoline.Bounce.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 /**
- *
  * @author ouertani
  */
 @RunWith(JUnit4.class)
 public class BounceTest {
-    
 
 
     private long regFactoriel(int n) {
         if (n < 2) return 1;
-        else return n * regFactoriel (n - 1);
+        else return n * regFactoriel(n - 1);
     }
-    
-    private BigInteger tailRecFact(int n ) {
-             return go(BigInteger.valueOf(n),BigInteger.ONE);
+
+    private BigInteger tailRecFact(int n) {
+        return go(BigInteger.valueOf(n), BigInteger.ONE);
     }
+
     private BigInteger safeFactoriel(int n) {
-        return (trampoline(safeGo(BigInteger.valueOf(n),BigInteger.ONE)));
+        return (trampoline(safeGo(BigInteger.valueOf(n), BigInteger.ONE)));
     }
-    
+
     private BigInteger go(BigInteger n, BigInteger acc) {
         if (n.intValue() < 2) return acc;
         else return go(n.subtract(BigInteger.ONE), acc.multiply(n));
     }
-    
+
     private Bounce<BigInteger> safeGo(BigInteger n, BigInteger acc) {
-        if( n.intValue() < 2) return Done(acc) ;
-        else return Call( () -> safeGo(n.subtract(BigInteger.ONE) , acc.multiply(n)));
+        if (n.intValue() < 2) return Done(acc);
+        else return Call(() -> safeGo(n.subtract(BigInteger.ONE), acc.multiply(n)));
     }
-  
-    @Test(expected=StackOverflowError.class)
+
+    @Test(expected = StackOverflowError.class)
     public void testStackOverFlow() {
         regFactoriel(99900);
     }
 
-    @Test(expected=StackOverflowError.class)
+    @Test(expected = StackOverflowError.class)
     public void regularFactoriel() {
-        tailRecFact(99900) ;
+        tailRecFact(99900);
     }
-    
+
     @Test()
     public void testNoStackOverFlow() {
-       
-         catchThrowable(safeFactoriel(99900));
-         assertFalse (caughtThrowable() instanceof StackOverflowError) ;
+
+        BigInteger safeFactoriel = safeFactoriel(99900);
+
+        assertTrue(safeFactoriel instanceof BigInteger);
     }
 
-    @Rule
-    public ErrorCollector collector = new ErrorCollector();
 
-    
-    
+
 }
